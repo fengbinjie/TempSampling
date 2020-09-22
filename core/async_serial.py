a75f02d116d133f85dffa1c98270226429c237f1
binjiefeng <31472267+fengbinjie@users.noreply.github.com>
2020-09-22 16:01:17 +0800

    add project info

--- core/async_serial.py
+++ core/async_serial.py
@@ -1,6 +1,13 @@
 import serial
 import serial.tools.list_ports
+import argparse
 
+__title__ = 'tempsampling'
+__version__ = '0.1.0'
+__build__ = 0x000100
+__author__ = 'Binjie Feng'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2020 Binjie Feng'
 
 def find_serial_port_list():
     return sorted(serial.tools.list_ports.comports())
