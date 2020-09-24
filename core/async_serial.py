eea8405a3b7f28cdb7be1c9bcc0d197b26434a1b
binjiefeng <31472267+fengbinjie@users.noreply.github.com>
2020-09-24 16:04:51 +0800

    basic class ComReadWrite

--- core/async_serial.py
+++ core/async_serial.py
@@ -66,8 +66,22 @@ class Com:
                 raise why
 
 class Protocol:
-    def __init__(self, path):
-        self._protocol = self._get_protocol_from_file(path)
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.fixed_token =(1,2,0xabcd)
+        self.reservered_token = (2,2,0x0000)
+        self.D_LEN = [3,1,None]
+        self.device_type = [4,1,None]
+        self.message_id = [5,2,None]
+        self.serial_number = [6,2,None]
+        self.client_id = [7,2,None]
+        self.DATA = [8,None,None]
+        self.CHECK = [9,1,None]
         pass
 
 
@@ -105,41 +119,79 @@ def open_file(path):
         file_handler.close()
         return
 
+class ComReadWrite:
+    def __init__(self):
+        self.com = Com()
+        self.protocol = Protocol()
+        self.lost_package_info = []
+        self.lag_package_info = []
+        self.ready_recv_pack_info = None
+        self.recv_data_process_flag = False
+        self.send_data_process_flag = False
+        self.next_send = 0
+
+    def receive_data_with_protocol(self):
+        ba = bytearray()
+        state = 1
+        temp_data_len = 0
+        len_token = 0
+        while self.com.com.inWaiting():
+            x = self.com.receive_data()
+            ch = ord(x)
+            if state == 1 and 0xcd == ch:
+                state = 2
+                ba.append(ch)
+            elif state == 2 and 0xab == ch:
+                state = 3
+                ba.append(ch)
+            elif state == 3:
+                ba.append(ch)
+                state = 4
+            elif state == 4:
+                ba.append(ch)
+                state = 5
+            elif state == 5:
+                len_token = ch
+                ba.append(ch)
+                state = 6
+                temp_data_len = 0
+            elif state == 6:
+                ba.append(ch)
+                temp_data_len += 1
+                if temp_data_len == len_token + 7:
+                    state = 7
+            elif state == 7:
+                ba.append(ch)
+            else:
+                raise Exception("串口数据接收错误")
+        return bytes(ba)
+
+    def send_data(self, data):
+        self.com.send_data(data)
+
+    def recv_data_process(self):
+        if self.recv_data_process_flag:
+            data = self.receive_data_with_protocol()
+            serial_num = data.serial_getserialnum()
+            if serial_num == self.ready_recv_pack_info:
+                self._update_recv_pack_info(None)
+            elif serial_num in self.lag_package_info:
+                self.lag_package_info.remove(serial_num)
+            else:
+                self.lost_package_info.append(serial_num)
 
 
+    def send_data_process(self, package):
+        if self.send_data_process_flag:
+            if self.ready_recv_pack_info:
+                self.lag_package_info.append(self.ready_recv_pack_info)
+            self.send_data(package)
+            self._update_recv_pack_info(package.get_seiralnum())
 
+            self.recv_data_process_flag = True
 
-def receive_data_with_protocol(com, protocol):
-    if type(com) is Com:
-        raise Exception("No Com")
-    if protocol:
-        serial_protocol = protocol['serial']
-        part_protocol_len =serial_protocol['device_type']['size']+\
-                           serial_protocol['message_id']['size']+\
-                           serial_protocol['serial_number']['size']+\
-                           serial_protocol['client_id']['size']
-        try:
-            current_position = 0x00
-            saved_data = bytearray()
-            data_len = 0
-            for token, properties in serial_protocol.items():
-                x = com.receive_data()
-                ch = ord(x)
-                if token == 'fixed_token' or token == 'reserverd_token':
-                    if properties['value'] != ch:
-                        break
-                if token == 'D_LEN':
-                    data_len = ch + part_protocol_len
-                if token ==
-
-
-        except:
-            pass
-    else:
-        raise Exception("there is no protocol")
-
-
-
+    def _update_recv_pack_info(self, new_pack_info):
+        self.ready_recv_pack_info = new_pack_info
 
 
 
