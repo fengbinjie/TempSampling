b4a1a13fb7d2e5c2b567942c10900c40dffc0773
binjiefeng <31472267+fengbinjie@users.noreply.github.com>
2020-10-21 16:23:59 +0800

    改进了发送方法，提升了效率，且更加可读

--- core/serial_with_protocol.py
+++ core/serial_with_protocol.py
@@ -6,13 +6,6 @@ import core.protocol as protocol
 import struct
 import queue
 
-STATENUM = protocol.protocol.header_fmt_size
-v = protocol.BASIC_PROTOCOL_PROPERTY["fixed_token"]["default_value"]
-fmt = f'{protocol.ENDIAN}{protocol.BASIC_PROTOCOL_PROPERTY["fixed_token"]["fmt"]}'
-fixed_state = struct.pack(fmt, v)
-
-firstState = fixed_state[0]
-secondState = fixed_state[1]
 
 # 状态机
 
@@ -40,61 +33,96 @@ class ReadWrite:
         return self.sendCount
 
     def recv_data_process(self):
-        ba = bytearray()
-        temp_data_len = 0
-        len_token = 0
+        # 创建回执
+        # 除数据外额外需要接受的包头信息长度
+        extra_fields_len = struct.calcsize(protocol.get_content_fmt2(with_endian=True))
+        # 获得固定标志位的默认值和加上字节顺序的fmt以及获得fmt的长度
+        known_fixed_token, fixed_token_fmt, fixed_token_fmt_len = protocol.get_fixed_token_property()
         while not self.receiveProgressStop:
-            ba.clear()
-            state = 0b00000001
-            while self.com.inWaiting():
-                # TODO: 去掉协议
-                # TODO: 创建一个类似zigbee中mtOSALSerialData_t的类
-                x = self.com.read(1)
-                ch = ord(x)
-                if state & 0b00000001:
-                    # 标志位1
-                    if 0xcd == ch:
-                        state <<= 1
-                        ba.append(ch)
-                elif state & 0b00000010:
-                    # 标志位2
-                    if 0xab == ch:
-                        state <<= 1
-                        ba.append(ch)
-                elif state & 0b00000100:
-                    # 得到长度
-                    state <<= 1
-                    len_token = ch
-                    ba.append(ch)
-                    temp_data_len = 0
-                elif state & 0b00001000:
-                    # 写入数据
-                    ba.append(ch)
-                    temp_data_len += 1
-                    if temp_data_len == len_token + 4:
-                        state <<= 1
-                elif state & 0b00010000:
-                    # 验证校验码正确
-                    if ord(pack_check_num(ba)) == ch:
+            # 有包写入
+            if self.com.inWaiting():
+                # 读取固定标志位（未知）
+                bytes_unknown_fixed_token = self.com.read(fixed_token_fmt_len)
+                # 解包固定标志位（未知）
+                unknown_fixed_token = struct.unpack(fixed_token_fmt, bytes_unknown_fixed_token)[0]
+                # 固定标志位（未知）确定是固定标志位
+                if unknown_fixed_token == known_fixed_token:
+                    # 读取数据长度
+                    data_len_fragment = self.com.read()
+                    # 读取数据片段（去固定标志位的消息头加上数据）
+                    data_fragment = self.com.read(ord(data_len_fragment)+extra_fields_len)
+                    # 拼接固定位、长度、数据得到完整的数据包
+                    package = bytes_unknown_fixed_token+data_len_fragment+data_fragment
+                    # 检查校验位
+                    if check(package) == ord(self.com.read()):
+                        # 创建回执
+                        receipt = protocol.parse_package(package)
                         self.feedBackCount += 1
-                        # TODO:转化为多字段的元祖，而不是现有的包
-                        fields = protocol.parse_package(bytes(ba))
-                        yield fields
-                        # self.read_buf.put(fields)
-                    else:
-                        break
-                else:
-                    state = 0b00000001
+                        yield receipt
             time.sleep(self.read_interval)
 
-    def pop_data(self):
-        return self.read_buf.get()
+    # def recv_data_process1(self):
+    #     ba = bytearray()
+    #     temp_data_len = 0
+    #     len_token = 0
+    #     while not self.receiveProgressStop:
+    #         ba.clear()
+    #         state = 0b00000001
+    #         while self.com.inWaiting():
+    #             # TODO: 去掉协议
+    #             # TODO: 创建一个类似zigbee中mtOSALSerialData_t的类，包含有效信息
+    #             x = self.com.read()
+    #             ch = ord(x)
+    #             if state & 0b00000001:
+    #                 # 标志位1
+    #                 if 0xcd == ch:
+    #                     state <<= 1
+    #                     ba.append(ch)
+    #             elif state & 0b00000010:
+    #                 # 标志位2
+    #                 if 0xab == ch:
+    #                     state <<= 1
+    #                     ba.append(ch)
+    #             # 数据部分
+    #             elif state & 0b00000100:
+    #                 # 得到长度
+    #                 state <<= 1
+    #                 len_token = ch
+    #                 ba.append(ch)
+    #                 temp_data_len = 0
+    #             elif state & 0b00001000:
+    #                 # 写入数据
+    #                 ba.append(ch)
+    #                 temp_data_len += 1
+    #                 if temp_data_len == len_token + 4:
+    #                     state <<= 1
+    #             elif state & 0b00010000:
+    #                 # 验证校验码正确
+    #                 if ord(pack_check_num(ba)) == ch:
+    #                     self.feedBackCount += 1
+    #                     # TODO:转化为多字段的元祖，而不是现有的包
+    #                     fields = protocol.parse_package(bytes(ba))
+    #                     yield fields
+    #                     # self.read_buf.put(fields)
+    #                 else:
+    #                     break
+    #             else:
+    #                 state = 0b00000001
+    #         time.sleep(self.read_interval)
 
     def send_data(self, node_addr, profile_id, serial_num, data=b''):
 
         if self.com.is_open:
             # 打包
-            package = protocol.complete_package(node_addr, profile_id, serial_num, data)
+            package = protocol.complete_package(data,
+                                                fixed_token=0xabcd,
+                                                data_len=len(data)+2,
+                                                node_addr=node_addr,
+                                                profile_id=profile_id,
+                                                serial_num=serial_num,
+                                                sub_fixed_token=0xcb,
+                                                sub_serial_num=serial_num,
+                                                )
             # 加上校验值
             package = package + pack_check_num(package)
             try:
