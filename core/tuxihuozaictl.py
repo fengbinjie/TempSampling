5f1007f959aff92b1055e755277790843d870864
binjiefeng <31472267+fengbinjie@users.noreply.github.com>
2020-12-01 15:15:53 +0800

    complete cmd skeleton

--- core/tuxihuozaictl.py
+++ core/tuxihuozaictl.py
@@ -3,15 +3,14 @@ import xmlrpc.client
 
 class Controller(cmd.Cmd):
 
-    def __init__(self, options, completekey='tab', stdin=None, stdout=None):
-        self.options = options
-        self.prompt = self.options.prompt + '>'
-        cmd.Cmd.__init__(completekey,stdin,stdout)
-        try:
-            self.proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
-        except:
-            exit()
-
+    def __init__(self, completekey='tab'):
+        super().__init__(completekey)
+        self.intro = "welcome"
+        self.prompt = 'tuxihuozaictl' + '>'
+        self.list_args = ['nodes','ports']
+        self.led_args = ['write','all']
+        self.temp_args = ['start','stop']
+        self.server_info = ''
 
     def emptyline(self):
         # 输入空行时，任何事都不做
@@ -20,24 +19,59 @@ class Controller(cmd.Cmd):
     def default(self, line):
         self.output(f'*** Unknown syntax: {line}')
 
-    # def exec_cmdloop(self, args, ):
-    #     pass
-    # def cmdloop(self, intro=None):
-    #     pass
-    def output(self, message):
-        if isinstance(message, str):
-            message = message.encode('utf-8')
-        self.stdout.write(message + '\n')
+    def do_temp(self,arg):
+        if arg in self.temp_args:
+            if arg == 'start':
+                print('start to sampling temp')
+            elif arg == 'stop':
+                print('stop sampling temp')
+        else:
+            self.help_temp()
 
-    # def do_start(self):
-    #     self.proxy.start()
-    def do_mul(self, *args):
-        self.proxy.mul(*args)
+    def do_led(self,arg):
+        if arg in self.list_args:
+            if arg == 'write':
+                print('write led sequence to node\'s rom')
+            elif arg == 'all':
+                print('Show all node-led mapping')
+        else:
+            self.help_list()
 
-    def do_list_ports(self):
-        self.proxy.list_ports()
+    def do_list(self, arg):
+        if arg in self.list_args:
+            if arg == 'nodes':
+                print('Show all nodes in zigbee Currently')
+            elif arg == 'ports':
+                print('Show serial ports list')
+        else:
+            self.help_list()
 
+    def help_list(self):
+        print('help: Show all nodes in zigbee Currently')
+
+    def help_led(self):
+        print("help: led")
+
+    def help_temp(self):
+        print("help: temp")
+
+    def completedefault(self, text, line, begidx, endidx):
+        # 确定命令存在
+        command = line.split()[0]
+        try:
+            getattr(self, 'do_' + command)
+        except AttributeError:
+            return [] # 没有该命令返回空
+        # 补全参数或打印全部参数
+        try:
+            completions = getattr(self,command+'_args')
+        except AttributeError:
+            # 没有参数返回空
+            return []
+        if text:
+            completions = [f for f in completions if f.startswith(text)]
+        return completions
 
 if __name__ == '__main__':
-    print(delims)
+    Controller().cmdloop()
 
--- test.py
+++ test.py
@@ -1,25 +1,8 @@
-import cmd
-import socketserver
-class ctr(cmd.Cmd):
-    def __init__(self):
-        super().__init__()
-        self.prompt = 'test>> '
-        self.intro = 'test for cmd'
+import subprocess
 
-    def emptyline(self):
-        return
-
-    def do_exe(self,arg):
-        if arg:
-            print(type(arg))
-            print(str(arg))
-
-    def complete_exe(self, text, line, begidx, endidx):
-        print(text,line,begidx,endidx)
+completed = subprocess.run(
+    ['ls', '-1'],
+)
+print('returncode:', completed.returncode)
 
 
-    def do_EOF(self,arg):
-        return True
-if __name__ == '__main__':
-    x = ctr()
-    x.cmdloop()
